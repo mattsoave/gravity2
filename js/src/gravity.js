55d22c22eb454bd791df34488e0c80d7cc19635f
mattsoave <mattsoave@gmail.com>
2017-03-12 12:21:34 -0700

    buncha changes

--- js/src/gravity.js
+++ js/src/gravity.js
@@ -1,4 +1,6 @@
-const gravity = 18;
+const gravity = 15;
+const SIZE_MULTIPLIER = 1.6;
+
 
 function Body(mass, coords, isRepeller) {
     var self = this;
@@ -10,10 +12,10 @@ function Body(mass, coords, isRepeller) {
         this.$el.appendTo($("body"));
         this.$el.addClass("body");
         this.$el.css({
-            top: 500 - self.coords.y,
-            left: 500 + self.coords.x,
-            height: self.mass * 2,
-            width: self.mass * 2
+            "top": 500 - self.coords.y,
+            "left": 800 + self.coords.x,
+            "height": self.mass * SIZE_MULTIPLIER,
+            "width": self.mass * SIZE_MULTIPLIER
         });
     }
 
@@ -28,6 +30,9 @@ function FixedBody(mass, coords, isRepeller) {
 function MovableBody(mass, coords, velocity, isRepeller) {
     Body.call(this, mass, coords, isRepeller);
     this.$el.addClass("planet");
+    this.$el.css({ 
+            "background-color": `rgb(0,${Math.round(100 + Math.random()*155)},${Math.round(100 + Math.random()*155)})`
+    });
 
     this.velocity = velocity;
 
@@ -56,8 +61,10 @@ function MovableBody(mass, coords, velocity, isRepeller) {
     }
 
     this.update = function () {
-        for (let fixedBodyIndex = 0; fixedBodyIndex < fixedBodies.length; fixedBodyIndex++) {
-            var other = fixedBodies[fixedBodyIndex];
+        for (let fixedBodyIndex of fixedBodies) {
+//        for (let fixedBodyIndex = 0; fixedBodyIndex < fixedBodies.length; fixedBodyIndex++) {
+//            var other = fixedBodies[fixedBodyIndex];
+            var other = fixedBodyIndex;
             var force = this.calcForceFromOther(other);
             var a = {};
             a.total = force.total / this.mass;
@@ -76,7 +83,7 @@ function MovableBody(mass, coords, velocity, isRepeller) {
 
         this.$el.css({
             top: 500 - this.coords.y,
-            left: 500 + this.coords.x
+            left: 800 + this.coords.x
         });
     }
 };
@@ -105,15 +112,18 @@ $(document).ready(function () {
 //        y: 400
 //    }, true));
     
-    fixedBodies.push(new FixedBody(30, {x: -300,y: -300}, true));
-    fixedBodies.push(new FixedBody(30, {x: 300,y: -300}, true));
-    fixedBodies.push(new FixedBody(30, {x: -300,y: 300}, true));
-    fixedBodies.push(new FixedBody(30, {x: 300,y: 300}, true));
-    fixedBodies.push(new FixedBody(30, {x: 450,y: 0}, true));
-    fixedBodies.push(new FixedBody(30, {x: -450,y: 0}, true));
-    fixedBodies.push(new FixedBody(30, {x: 0,y: 450}, true));
-    fixedBodies.push(new FixedBody(30, {x: 0,y: -450}, true));
-    fixedBodies.push(new FixedBody(20, {x: 0,y: 0}, true));
+//    fixedBodies.push(new FixedBody(30, {x: -300,y: -300}, true));
+//    fixedBodies.push(new FixedBody(30, {x: 300,y: -300}, true));
+//    fixedBodies.push(new FixedBody(30, {x: -300,y: 300}, true));
+//    fixedBodies.push(new FixedBody(30, {x: 300,y: 300}, true));
+//    fixedBodies.push(new FixedBody(30, {x: 450,y: 0}, true));
+//    fixedBodies.push(new FixedBody(60, {x: -450,y: 0}, true));
+//    fixedBodies.push(new FixedBody(30, {x: 0,y: 450}, true));
+//    fixedBodies.push(new FixedBody(30, {x: 0,y: -450}, true));
+//    fixedBodies.push(new FixedBody(20, {x: 0,y: 0}, true));
+    
+        fixedBodies.push(new FixedBody(30, {x: 0,y: 0}, false));
+//        fixedBodies.push(new FixedBody(1, {x: -300,y: 300}, true));
 
 
 //    movableBodies.push(new MovableBody(5, {
@@ -134,9 +144,20 @@ $(document).ready(function () {
     
     
     
-    for (let a = 0; a < 20; a++) {
+//    for (let a = 0; a < 20; a++) {
 //        movableBodies.push(new MovableBody(5, {x: 0, y: 180}, {x: -1.3 + a*(2/10), y: 0}, false));
-        movableBodies.push(new MovableBody(5, {x: a*10, y: 200}, {x: 1, y: -.5}, false));
+//    }
+    
+//    for (let a = 0; a < 20; a++) {
+//        movableBodies.push(new MovableBody(5, {x: a*10, y: 200}, {x: 1, y: -.5}, false));
+//    }
+    
+    for (let a = 0; a < 1; a++) {
+        for (let b = 0; b < 30; b++) {
+//            movableBodies.push(new MovableBody(2 + Math.random()*10, {x: a*5, y: b*20 + 250}, {x: 1 , y: 0}, false));    
+//            movableBodies.push(new MovableBody(2 + Math.random()*10, {x: a*5, y: 300}, {x: 1 , y: -.5 + .1*b}, false));    
+            movableBodies.push(new MovableBody(2 + Math.random()*10, {x: a*5, y: 300}, {x: 1 , y: -1 + Math.random()*2}, false));    
+        }
     }
     
 //    movableBodies.push(new MovableBody(5, {x: 40, y: 180}, {x: 0, y: 0}, false));
